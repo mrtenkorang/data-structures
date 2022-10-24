36493707ac876fa4591a2e85730a8efd8b3eec34
Tenkorang17 <josephoforitenkorang390@gmail.com>
2022-10-24 16:09:34 +0000

    Queue Implementation

--- Queue/C-implementation/queue.c
+++ Queue/C-implementation/queue.c
@@ -1,3 +1,9 @@
+/*
+In a queue data structure, insertion is done at the bottom while deletion is done from the 
+front of the structure. this is so because in a queue whatever goes in first comes out first like 
+it os experienced in real life
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,10 +28,43 @@ struct Employee* enQueue(struct Employee* head, int age, double salary){
 	struct Employee* temp = head;
 
 	if (head == NULL){
-		
+		head = emp;
+		return head;
 	}
+	while (temp->next != NULL){
+		temp = temp->next;
+	}
+	temp->next = emp;
+	return head;
+}
+
+void Print(struct Employee* head){
+	if (head == NULL){
+		return;
+	}
+	printf("%d  %.1lf\n",head->age, head->salary);
+	Print(head->next);
+}
+
+void front(struct Employee* head){
+	printf("%d  %.1lf\n", head->age, head->salary);
+}
+int isEmpty(struct Employee* head){
+	int check = 0;
+	if (head == NULL){
+		check = 1;
+	}
+	return check;
 }
 
 int main(){
+	struct Employee* head;
+	head = enQueue(head,22, 20000);
+	head = enQueue(head,23, 30000);
+	head = enQueue(head,24, 40000);
+	head = enQueue(head,25, 50000);
 
+	front(head);
+	printf("%d\n",isEmpty(head));
+	//Print(head);
 }
